3424a7b846eeb932d692699eafcc6e00043f0922
Ilhom <ilhom@example.com>
2024-07-03 08:17:55 +0500

    homework

--- src/routes/home/products/single-page/Single.jsx
+++ src/routes/home/products/single-page/Single.jsx
@@ -1,10 +1,12 @@
 import "./Single.css"
 import {Link, useParams} from "react-router-dom";
 import {cardInfo} from "../../../../utils/fakeData.js";
+import {FaStar} from "react-icons/fa";
+import {FaStarHalfStroke} from "react-icons/fa6";
 
 const Single = () => {
     const {id} = useParams()
-    const food = cardInfo.filter(food => food.id === parseInt(id))
+    const food = cardInfo.find(food => food.id === parseInt(id))
     if (!food) {
         return <div>Food Not Found</div>
     }
@@ -15,11 +17,17 @@ const Single = () => {
                 <Link to="/products">Back...</Link>
             </div>
             <div className='single-card'>
-                <div>
-                    <img style={{width: "100px"}} src={food.img} alt={food.name}/>
+                <div className="single-img">
+                    <img src={food.img} alt={food.name}/>
                 </div>
-                <div>
+                <div className="single-card-body">
                     <h2>{food.name}</h2>
+                    <p className="desc">{food.description}</p>
+                    <div className="bottom">
+                        <p><span><FaStar/></span> <span><FaStar/></span> <span><FaStar/></span>
+                            <span><FaStar/></span> <span><FaStarHalfStroke/> </span> {food.rating}</p>
+                        <p>${food.price}</p>
+                    </div>
                 </div>
             </div>
         </div>
